f4c27628d7db7fb81e6634137f6fb613eaec23be
michaeljqzq <michaeljqzq@gmail.com>
2018-03-09 21:51:46 +0800

    update

--- modules/filtermodule3/Program.cs
+++ modules/filtermodule3/Program.cs
@@ -119,7 +119,7 @@ namespace filtermodule
                 {
                     pipeMessage.Properties.Add(prop.Key, prop.Value);
                 }
-                pipeMessage.Properties.Add("VSTS", "zhiqing");
+                pipeMessage.Properties.Add("VSTSm3", "zhiqing");
                 await deviceClient.SendEventAsync("output1", pipeMessage);
                 Console.WriteLine("Received message sent");
             }
